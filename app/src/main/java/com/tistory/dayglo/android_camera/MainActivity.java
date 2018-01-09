cbba35addf0257972564a5274ad1a6a5a07199c7
ChanJoo <sksckswn@naver.com>
2018-01-09 23:51:57 +0900

    Take a photo with a camera app

--- app/src/main/java/com/tistory/dayglo/android_camera/MainActivity.java
+++ app/src/main/java/com/tistory/dayglo/android_camera/MainActivity.java
@@ -1,13 +1,23 @@
 package com.tistory.dayglo.android_camera;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
